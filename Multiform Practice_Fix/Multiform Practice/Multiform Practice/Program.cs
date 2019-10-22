ebbe37b7c99f0875b278fd3b40ca90fe24c906eb
whitechanlder73 <whitechandler73@gmail.com>
2019-10-22 14:38:37 -0500

    added finished multiform practice project

--- Multiform Practice_Fix/Multiform Practice/Multiform Practice/Program.cs
+++ Multiform Practice_Fix/Multiform Practice/Multiform Practice/Program.cs
@@ -16,7 +16,7 @@ namespace Multiform_Practice
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
