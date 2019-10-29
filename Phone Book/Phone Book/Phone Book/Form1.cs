fb7d0e1014ddbc7072a2fb2ba5431a6ec5af11d3
whitechanlder73 <whitechandler73@gmail.com>
2019-10-29 11:44:45 -0500

    Finished project

--- Phone Book/Phone Book/Phone Book/Form1.cs
+++ Phone Book/Phone Book/Phone Book/Form1.cs
@@ -16,5 +16,25 @@ namespace Phone_Book
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'personDataSet.Person' table. You can move, or remove it, as needed.
+            this.personTableAdapter.Fill(this.personDataSet.Person);
+
+        }
+
+        private void personBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.personBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.personDataSet);
+
+        }
+
+        private void personDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
